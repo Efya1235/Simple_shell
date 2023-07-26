b441713382c428916ce350d06ee7d21092e3cdc2
Mordzifah99 <mordzifahbenedicta99@gmail.com>
2023-07-26 05:29:04 -0700

    more custom functions

--- cust_line.c
+++ cust_line.c
@@ -1,34 +1,80 @@
 #include "shell.h"
 
 /**
- * custom_getline - function that works as the standard deadline function
- * @lineptr: pointer to the array
- * @n: hold the size of the memory
- * @stream: location or file read from
- * Return: number of character read
+ * _strcmp - comparing two strings
+ * @m1: A pointer to the first string to be compared
+ * @m2: A pointer to the second string to be compared
+ * Return: if str1 == str2, 0;
+ * if str1 > str2, the positive difference of the unmatched characters
+ * if str1 < str2, the negative difference of the firt unmatched char
  */
-
-ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
+int _strcmp(char *m1, char *m2)
 {
-	size_t buffer_size = *n;
-	ssize_t bytes_read;
+	while (*m1 && *m2 && *m1 == *m2)
+	{
+		m1++;
+		m2++;
+	}
+	return (*m1 - *m2);
+}
 
-	if (lineptr == NULL || n == NULL || stream == NULL)
+/**
+ * _strcat - concatenating two strings
+ * @dest: the destination string
+ * @src: the source string
+ * Return: A pointer to the resulting string destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	size_t destlen = strlen(dest);
+	size_t srclen = strlen(src);
+	char *result = (char *)malloc((destlen + srclen + 1) * sizeof(char));
+
+	if (result == NULL)
 	{
-		return (-1);
+		fprintf(stderr, "Memory allocation error\n");
+		return (NULL);
 	}
-	*lineptr = (char *)malloc(buffer_size * sizeof(char));
-	if (*lineptr == NULL)
+	strcpy(result, dest);
+	strcat(result, src);
+	return (result);
+}
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to pe copied
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	char *result = (char *)malloc(n * sizeof(char));
+
+	if (result == NULL)
 	{
-		return (-1);
+		fprintf(stderr, "Memory allocation error\n");
+		return (NULL);
 	}
-	bytes_read = getline(lineptr, &buffer_size, stream);
-	if (bytes_read == -1)
+
+	for (i = 0; n > 1; i++)
 	{
-		free(*lineptr);
-		*lineptr = NULL;
-		return (-1);
+		result[i] = src[i];
 	}
-	*n = buffer_size;
-	return (bytes_read);
+
+	memcpy(dest, result, n);
+	free(result);
+	return (dest);
+}
+
+/**
+ * _isdigit - checker for digit characters
+ * @j: the character to be checked
+ * Return: 1 for a digit character, 0 otherwise
+ */
+int _isdigit(int j)
+{
+	return (j >= '0' && j <= '9');
 }
